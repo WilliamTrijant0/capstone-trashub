52ab5d1fd4c529bb464712bcef7f50a7d855bbb6
mp38590 <mp38590@gmail.com>
2024-05-30 20:23:36 +0700

    FE Trashub

---
+++ app/src/main/java/com/android/trashub/custom/MyEditText.kt
@@ -0,0 +1,115 @@
+package com.android.storyapp.custom
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.android.storyapp.R
+
+open class MyEditText : AppCompatEditText, View.OnTouchListener {
+
+    private lateinit var clearButtonImage: Drawable
+    private val clearButtonMarginStart = 65
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawablesRelative[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart + clearButtonMarginStart).toFloat()
+                if (event.x < clearButtonEnd) isClearButtonClicked = true
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth - clearButtonMarginStart).toFloat()
+                if (event.x > clearButtonStart) isClearButtonClicked = true
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+                        if (text != null) text?.clear()
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else {
+                return false
+            }
+        }
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (compoundDrawablesRelative[2] != null) {
+            val drawable = compoundDrawablesRelative[2]
+            val drawablePadding = compoundDrawablePadding
+            val drawableSize = drawable.intrinsicWidth
+            val totalWidth = drawableSize + drawablePadding
+            val endPadding = clearButtonMarginStart
+
+            setPadding(paddingLeft, paddingTop, totalWidth + endPadding, paddingBottom)
+        }
+    }
+}
---
+++ app/src/main/java/com/android/trashub/custom/MyNameCustom.kt
@@ -0,0 +1,39 @@
+package com.android.storyapp.custom
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import com.android.storyapp.R
+
+class MyNameCustom : MyEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length in 1..1) {
+                    error = resources.getString(R.string.min_length_name, 1)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+}
